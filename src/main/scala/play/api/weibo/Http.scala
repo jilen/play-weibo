74e8c8aa367572efec75587f51b34dfff973f2ee
jilen <jilen.zhang@gmail.com>
2014-01-22 01:14:11 +0800

    add cycles

--- src/main/scala/play/api/weibo/Http.scala
+++ src/main/scala/play/api/weibo/Http.scala
@@ -1,5 +1,19 @@
 package play.api.weibo
 
+import scala.concurrent.Future
+/**
+ * Http transport abstract
+ */
 trait Http {
+  def get(params: Map[String, Any]): Future[String]
+  def post(param: Map[String, Any]): Future[String]
+}
 
+/**
+ * Http constants defines
+ */
+object Http {
+  sealed trait Method
+  case object GET extends Method
+  case object POST extends Method
 }
--- src/main/scala/play/api/weibo/WeiboApi.scala
+++ src/main/scala/play/api/weibo/WeiboApi.scala
@@ -1,18 +1,58 @@
 package play.api.weibo
 
-
-
+import scala.concurrent.duration.Duration
+import scala.concurrent.{ Await, Future }
+import play.api.libs.concurrent.Execution.Implicits._
 /**
  * weibo api abstraction
  */
 trait Api[R] {
-  def execute : Future[Either[WeiboApiError, R]]
+  def execute(): Future[Either[WeiboApiError, R]]
 }
 
-trait GenericHttpApi[R] extends Api[R] with Http {
-  def params: Map[String, Any]
-  def parse(body: String): Either[WeiboApiError, R]
+trait ApiReader[T <: Api[_]] {
+  def read[T](api: T): Map[String, Any]
 }
+
+trait ApiParser[R] {
+  def parse(body: String):  Either[WeiboApiError, R]
+}
+
+trait Protocol[A <: Api[R], R] extends ApiReader[A] with ApiParser[R]  {
+}
+
+trait GenericHttpApi[R, T <: Api[R]] extends Api[R] with Http {
+  import Http._
+
+  def protocol: Protocol[T, R]
+  def method: Method
+
+  def execute() = {
+    val params = protocol.read(this)
+    method match {
+      case GET => get(params).map(protocol.parse)
+      case POST => post(params).map(protocol.parse)
+    }
+  }
+}
+
+trait HttpApi[R, T <: Api[R]]
+    extends GenericHttpApi[R, T] {
+  implicit val protocol: Protocol[T, R]
+}
+
+
+abstract class HttGetApi[R, A <: Api[R]](implicit protocol: Protocol) extends HttpApi[R, A] { self: A =>
+  val method = Http.GET
+}
+
+trait HttpPostApi[R, A <: Api[R]](implicit protocol: Protocol) extends HttpApi[R, A] { self: A =>
+  val method = Http.POST
+}
+
+
+
+
 
 case class WeiboApiError(
   error: String,
